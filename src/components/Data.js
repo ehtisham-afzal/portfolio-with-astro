86d125294eef9124282c69be29c31bbff45773eb
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-29 17:40:07 +0500

    Added profile links and changed Background Changes to be committed: 	deleted:    public/BackgroundSVG.svg 	modified:   src/components/Background.astro 	new file:   src/components/Data.js 	new file:   src/components/Github.astro 	new file:   src/components/LinkedIn.astro 	new file:   src/components/TwitterX.astro 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro

---
+++ src/components/Data.js
@@ -0,0 +1,103 @@
+export const SocialLinks = [
+    {
+        Name: "LinkIn",
+        IdURL: "https://www.linkedin.com/in/ehtisham-afzal-819593195/",
+        IconURL:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg",
+    },
+    {
+        Name: "Github",
+        IdURL: "https://github.com/EHTISHAM-Afzal",
+        IconURL:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    {
+        Name: "Twitter",
+        IdURL: "https://twitter.com/EHTISHAMoo7",
+        IconURL:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg",
+    },
+];
+
+export const AppLogos = [
+    {
+        Name: "HTML",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    },
+    {
+        Name: "CSS ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    },
+    {
+        Name: "JavaScript",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    },
+    {
+        Name: "React",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+    {
+        Name: "Git ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+    {
+        Name: "Github",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    {
+        Name: "Npm ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
+    },
+    {
+        Name: "Yarn",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg",
+    },
+    {
+        Name: "NextJs",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+    },
+    {
+        Name: "TailwindCSS",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    },
+    {
+        Name: "Redux",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+    },
+    {
+        Name: "Jest",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+    },
+    {
+        Name: "Express",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+    },
+    {
+        Name: "NodeJS ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    },
+    {
+        Name: "MongoDB",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg",
+    },
+    {
+        Name: "MarkDown",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg",
+    },
+    {
+        Name: "VSCode",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+    },
+    {
+        Name: "Figma ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    },
+    {
+        Name: "Babel ",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg",
+    },
+    {
+        Name: "Bash",
+        Logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+    },
+];
